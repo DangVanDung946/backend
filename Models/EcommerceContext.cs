7520d97794bccea2b6e2299b7dbf9505e7c2b58e
DangVanDung946 <dungdang.130500@gmail.com>
2023-10-11 12:27:27 +0700

    update models

---
+++ Models/EcommerceContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace backend.Models
+{
+    public class EcommerceContext : DbContext
+    {
+        public EcommerceContext(DbContextOptions<EcommerceContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Product> MoviesList { get; set; } = null!;
+    }
+}
+
+
